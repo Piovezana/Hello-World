697389236d1bbb7c68e364d6f2d92c9f972d60ed
Victor Piovezana <piovezanavictor@gmail.com>
2026-02-06 16:13:41 -0300

    add hello world HTML

---
+++ Hello Design/DesignHW.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Hello_Design
+{
+    class DesignHW
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Desing!!!");
+            Console.ReadKey();
+
+        }
+    }
+}
